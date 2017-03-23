639f06b738580296df494b64e6290c1165e3587a
anna-larina <anna_larina_99@bk.ru>
2017-03-23 22:50:28 +0300

    Create 3.c

---
+++ cs_17_spring/3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main ()
+
+{ 
+    float a, b, c, d, x, y;
+    printf ("Введите комплексное сопростивление a+bi, c+di :\n");
+    scanf("%f+%fi, %f+%fi", &a, &b, &c, &d);
+    x = ((a*a*c+a*c*c+a*d*d+c*b*b)/(a*a+2*a*c+c*c+b*b+2*b*d+d*d));
+    y = ((a*a*d+b*c*c+b*b*d+d*d*b)/(a*a+2*a*c+c*c+b*b+2*b*d+d*d));
+    printf("%.2f+%.2fi", x, y);
+    
+    return 0
+    
+    
+    
+}
