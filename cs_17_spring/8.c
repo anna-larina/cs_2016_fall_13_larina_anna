491e8e752d9f560acd39f0389970241db6ee6c41
anna-larina <anna_larina_99@bk.ru>
2017-03-25 00:18:23 +0300

    Create 8.c

---
+++ cs_17_spring/8.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float a, b, c, d, x, x1, x2;
+    printf("Введите коэффициент а: ");
+    scanf("%f", a);
+    printf("Введите коэффициент b: ");
+    scanf("%f", b);
+    printf("Введите коэффициент c: ");
+    scanf("%f", c);
+    
+    d = b*b-4*a*c;
+     
+    if (d < 0)
+    {
+        printf("Нет вещественных корней!\n");
+    }
+    if (d == 0)
+    {
+        x=-b/(2*a);
+        printf("x = %.2f", x);
+    }
+    
+    if (d >0)
+    {
+        x1 = (-b + sqrt(d))/(2*a);
+        x2 = (-b - sqrt(d))/(2*a);
+        printf("x1 = %.2f\nx2 = %.2f\n", x1, x2);
+    }
+    return 0;
+}
