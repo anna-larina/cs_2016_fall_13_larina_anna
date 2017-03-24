d17eabf32e436e8b1a1b35dccf19279b91d5fb96
anna-larina <anna_larina_99@bk.ru>
2017-03-24 20:38:06 +0300

    Update 5.c

--- cs_17_spring/5.c
+++ cs_17_spring/5.c
@@ -2,23 +2,24 @@
 
 int main()
 {
-float c, m, f;
-printf( "Введите нижнюю границу:");
-scanf("%f", &c);
-printf("Введите верхнюю границу:");
-scanf("%f", &m);
-printf("   C    |    F   \n");
-if (c <= m)
-{
-    while (c <= m)
-    {
-        f = 1.8*c+32;
-        printf(" %.3f | %.3f \n", c, f);
-        c +=1; 
-    }
-} 
-else 
-{printf( "Неверные данные!!!\n");
-}
-return 0;
+    float c, m, f;
+    printf( "Введите нижнюю границу:");
+    scanf("%f", &c);
+    printf("Введите верхнюю границу:");
+    scanf("%f", &m);
+    printf("   C    |    F   \n");
+    if (c <= m)
+        {
+        while (c <= m)
+            {
+                f = 1.8*c+32;
+                printf(" %.3f | %.3f \n", c, f);
+                c +=1; 
+            }
+        } 
+    else 
+        {
+        printf( "Неверные данные!!!\n");
+        }
+    return 0;
 }
