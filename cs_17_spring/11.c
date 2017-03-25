c03669d4e7a8a8968e4b36daad4f837bc813f2c5
anna-larina <anna_larina_99@bk.ru>
2017-03-25 12:34:16 +0300

    Create 11.c

---
+++ cs_17_spring/11.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <locale.h>
+
+int main()
+{
+setlocale(LC_CTYPE, "Russian");
+    int i, k, S;
+    char z[1000];
+    printf("Vvedite chislo:");
+    
+    gets(z);
+
+    for(k=strlen(z);k>0;--k)
+    {
+    if(z[k-1]=='1')
+    {
+      S=S+pow(2,i);
+
+      }
+     i=i+1;
+    } 
+    printf("%d", S);
+    return 0;
+}
