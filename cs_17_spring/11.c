7e4ee9a586a7a93bb8979756a888ce755e7f2eff
anna-larina <anna_larina_99@bk.ru>
2017-03-25 18:14:25 +0300

    Update 11.c

--- cs_17_spring/11.c
+++ cs_17_spring/11.c
@@ -10,17 +10,16 @@ setlocale(LC_CTYPE, "Russian");
     int i, k, S;
     char z[1000];
     printf("Введите число:");
-    
     gets(z);
 
-    for(k=strlen(z);k>0;--k)
+    for(k = strlen(z); k > 0; --k)
     {
     if(z[k-1]=='1')
     {
-      S=S+pow(2,i);
+      S = S + pow(2,i);
 
       }
-     i=i+1;
+     i = i + 1;
     } 
     printf("%d", S);
     return 0;
