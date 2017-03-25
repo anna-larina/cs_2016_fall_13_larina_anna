8483e699d8ac7fa5a8555a75185dd9ec90c4dc1a
anna-larina <anna_larina_99@bk.ru>
2017-03-25 12:43:58 +0300

    Update 2.c

--- cs_17_spring/2.c
+++ cs_17_spring/2.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main()
-{
+{setlocale(LC_CTYPE, "Russian");
+ 
  int day, month, x;
  printf("Введите дату рождения через пробел\n");
  scanf("%i %i", &day, &month);
