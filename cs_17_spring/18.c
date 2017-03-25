ff9e900b93eaa89160a0077d9eb4b71be0379f6f
anna-larina <anna_larina_99@bk.ru>
2017-03-25 18:03:51 +0300

    Create 18.c

---
+++ cs_17_spring/18.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+ void copy(char *a,char *b)
+{ 
+  char str[1000];
+  FILE *A,*B;
+ 
+  A = fopen(a,"r");
+  B = fopen(b,"w");
+  char i;
+  while((i=fgetc(A))!=EOF)
+    {
+      fputc(i,B);
+    }
+}
+ 
+  int main()
+  {
+  	setlocale(LC_CTYPE, "Russian");
+    char x[1000],y[1000];
+    printf("Путь к файлу, из которого копировать:\n");
+    gets(x);
+    printf("Путь к файлу, куда копировать:\n");
+    gets(y);
+    copy(x,y);
+    printf("Готово!");
+  return 0;
+}
