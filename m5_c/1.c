a0292b3d64ca8feb7393fd67488813d7ef4314ef
anna-larina <anna_larina_99@bk.ru>
2017-05-31 02:14:00 +0300

    Update 1.c

--- m5_c/1.c
+++ m5_c/1.c
@@ -11,9 +11,9 @@ int main()
 {
 	setlocale(LC_CTYPE, "Russian");
 	float h, x, a, b;
-	float P = 0;
-	float P1 = 0;
-	float P2 = 0;
+	float D = 0;
+	float D1 = 0;
+	float D2 = 0;
   
 	 printf(" Этот метод реализует численное дифференцирование по трем точкам функции y=x^2. \n");
 	printf("Введите нижнюю границу интервала: ");
@@ -23,14 +23,14 @@ int main()
 	printf("Введите шаг: ");
 	scanf("%f", &h);
 	x = a;
-	P1 = (1 / (2 * h))*((-3) * mainFunction(x) + 4 * (mainFunction(x + h)) - (mainFunction(x + 2 * h)));
-	P2 = (1 / (2 * h))*(-mainFunction(x) + mainFunction(x + 2 * h));
-	printf("%.3f\n", P1);
-	printf("%.3f\n", P2);
+	D1 = (1 / (2 * h))*((-3) * mainFunction(x) + 4 * (mainFunction(x + h)) - (mainFunction(x + 2 * h)));
+	D2 = (1 / (2 * h))*(-mainFunction(x) + mainFunction(x + 2 * h));
+	printf("%.3f\n", D1);
+	printf("%.3f\n", D2);
 	while (x + 2 * h <= b) {
-		P = (1 / (2 * h))*(mainFunction(x) - 4 * (mainFunction(x + h)) + 3 * (mainFunction(x + 2 * h)));
+		D = (1 / (2 * h))*(mainFunction(x) - 4 * (mainFunction(x + h)) + 3 * (mainFunction(x + 2 * h)));
 		x = x + h;
-		printf("%.3f\n", P);
+		printf("%.3f\n", D);
 	}
 	return 0;
 }
